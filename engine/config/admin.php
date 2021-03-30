6e9656c034ebaf1356d1abd94cf99e1f9774100b
NoiStudio <artem@noibiz.com>
2021-03-30 17:49:18 +0400

    Create admin.php

---
+++ engine/config/admin.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    "template_migration_remove_up" => '
+    $field=[field];
+    $table = \content\models\TableConfig::get(%name_table%);
+    \content\models\TableConfig::deleteField($table, $field);
+    ',
+    "template_migration_remove_down" => '
+    $table = \content\models\TableConfig::get(%name_table%);
+      $newtable = \db\JsonQuery::get($table["name"], "tables", "name");
+
+        $fields = json_decode($newtable->fields, true);
+         $json_data = [JSON_DATA];
+         $table_data = json_decode($json_data, true);
+         foreach($table_data as $name_field=>$new_field){
+              $fields[$name_field] = $new_field;
+            \content\models\TableConfig::addField($newtable, $name_field, $new_field);
+            
+     
+        }
+        $newtable->fields = json_encode($fields);
+        $newtable->save();
+    ',
+    "template_migration_update_down" => '
+         $table = \content\models\TableConfig::get(%name_table%);
+         $json_data = [JSON_DATA];
+         $table_data = json_decode($json_data, true);
+         foreach($table_data as $name_field=>$new_field){
+        if (is_array($table) and isset($name_field) and is_string($name_field) > 0 and isset($table["fields"][$name_field])) {
+            \content\models\TableConfig::deleteField($table, $name_field);
+        }
+        }
+  ',
+    "template_migration_update_up" => '$json_data = [JSON_DATA];
+
+        $table_data = json_decode($json_data, true);
+            
+        $newtable  = \db\JsonQuery::get(%name_table%, "tables", "name");
+        
+        $fields=json_decode($newtable->fields,true);
+        foreach($table_data as $name_field=>$new_field){
+        $fields[$name_field]=$new_field;
+        \content\models\TableConfig::addField($newtable, $name_field, $new_field); 
+        } 
+        $newtable->fields = json_encode($fields);
+        
+
+        $newtable->save();',
+    "template_migration_down" => '\content\models\TableConfig::delete(%name_table%);',
+
+    "template_migration_up" => '
+   $json_data = [JSON_DATA];
+
+        $table_data = json_decode($json_data, true);
+
+        $newtable = \db\JsonQuery::insert("tables");
+        $newtable->name = $table_data["name"];
+        $newtable->fields = json_encode($table_data["fields"]);
+        $newtable->title = $table_data["title"];
+        $newtable->count = $table_data["count"];
+        $newtable->sort_field = $table_data["sort_field"];
+        $newtable->sort_type = $table_data["sort_type"];
+        \content\models\TableConfig::createTable($newtable);
+
+        $newtable->save();
+  ',
+];
